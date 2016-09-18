0bdaef8294ea7cfe8922b5ad268233097605eafa
Ratanak Kim <ratanak.kim@student.unsw.edu.au>
2016-09-18 22:52:11 +1000

    Revert "Make the google mapsshow"
    This reverts commit a6e256241cccda2e746e03b0cc899a3b569d90d1.

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -1,30 +1,15 @@
-function renewMap(coordinate){
-  var mapObj = new google.maps.Map(document.getElementById('googleMap'), {
-    zoom: 18,
-    center: coordinate,
-  });
-  return mapObj;
-}
-
-function getJSON(url){
-  var Httpreq = new XMLHttpRequest(); // Init a request
-  Httpreq.open("GET", url,false);
-  Httpreq.send(null);
-  return Httpreq.responseText;
-}
-
-function getCoordinate(ip){
-  var json_str = getJSON("http://freegeoip.net/json/" + ip); // Send request to freegeoip to get json string -> hence parse it to a json object;
-  var json = JSON.parse(json_str);
-  var coordinate = {lat: json.latitude, lng: json.longitude};
-  return coordinate;
-}
-
-function getMarker(hop){
-  var coordinate = getCoordinate(hop);
-  var marker = new google.maps.Marker({
-    position: coordinate,
-    animation: google.maps.Animation.DROP,
-  });
-  return marker;
-}
+var HelloWorld = React.createClass({
+  render: function() {
+    return (
+      <p>
+        Hello, it is {this.props.date.toTimeString()}
+      </p>
+    );
+  }
+});
+setInterval(function() {
+  ReactDOM.render(
+    <HelloWorld date={new Date()} />,
+    document.getElementById('example')
+  );
+}, 500);
