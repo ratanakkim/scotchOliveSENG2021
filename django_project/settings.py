1d9f1ede1a003310fce0e2d5b583e9434408a3f7
N C <N C>
2016-09-19 18:26:17 +1000

    fixed referencing static files in the html

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django_app',
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -116,5 +117,9 @@ USE_L10N = True
 USE_TZ = True
 
 
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+STATIC_URL = '/static/'
