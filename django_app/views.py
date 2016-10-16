88760ad582ccf876033af161476014d2f42f6293
Ratanak Kim <ratanak.kim@student.unsw.edu.au>
2016-10-16 20:33:10 +1100

    Added categories page. Access by  http://127.0.0.1:8000/category/

--- django_app/views.py
+++ django_app/views.py
@@ -4,3 +4,5 @@ def index(request):
    return render(request, "index.html", {})
 def initmap(request):
     return render(request, "map.html", {})
+def categories0(request):
+    return render(request, "categories0.html", {})
